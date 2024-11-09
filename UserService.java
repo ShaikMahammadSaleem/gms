e2f63745e67d77299a6bfde6439576278a6587ec
ShaikMahammadSaleem <Shaiksaleem9608@gmail.com>
2024-11-09 08:44:59 +0530

    Add files via upload

---
+++ UserService.java
@@ -0,0 +1,28 @@
+package com.example.gms.service;
+
+
+import com.gms.entity.User;
+import com.gms.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+    
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+    
+    public User saveUser(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        return userRepository.save(user);
+    }
+    
+    public User findByUsername(String username) {
+        return userRepository.findByUsername(username)
+                .orElse(null);
+    }
+}
